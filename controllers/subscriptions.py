db9e8847f3247fafe7ea91998ee879ec16d6b7fd
robertschaap <robert.schaap@mac.com>
2020-06-26 14:22:27 +0200

    refactor(subscriptions): get stubs from util

--- controllers/subscriptions.py
+++ controllers/subscriptions.py
@@ -1,15 +1,8 @@
 from flask import Blueprint
 from utils.api import api_response, api_res_type
+from utils.stubs import get_subscriptions_stub
 
 subscriptions_controller = Blueprint('subscriptions_controller', __name__)
-
-def get_subscriptions_stub():
-    import json
-
-    with open("stubs/subscriptions.json") as subscriptions_stub:
-        subscriptions_json = subscriptions_stub.read()
-
-    return json.loads(subscriptions_json)
 
 @subscriptions_controller.route('/api/subscriptions')
 def get_subscriptions():
