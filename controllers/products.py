7d9071c2d4ce45d1edcd400c02bf53f946c85f15
robertschaap <robert.schaap@mac.com>
2020-06-26 14:22:04 +0200

    refactor(products): get stubs from util

--- controllers/products.py
+++ controllers/products.py
@@ -1,15 +1,8 @@
 from flask import Blueprint
 from utils.api import api_response, api_res_type
+from utils.stubs import get_products_stub
 
 products_controller = Blueprint('products_controller', __name__)
-
-def get_products_stub():
-    import json
-
-    with open("stubs/products.json") as products_stub:
-        products_json = products_stub.read()
-
-    return json.loads(products_json)
 
 @products_controller.route('/api/products')
 def get_products():
