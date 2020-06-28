4328481e4f9d2b2305e2af0ba181eba93229afe6
robertschaap <robert.schaap@mac.com>
2020-06-28 11:14:30 +0200

    feat(server): change api response

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
-from controllers.products import products_controller, get_products
-from controllers.product import product_controller
-from controllers.subscriptions import subscriptions_controller
 from flask import Flask
-from utils.api import api_response, api_res_type
+from controllers.product import product_controller
+from controllers.products import products_controller, get_products
+from controllers.subscriptions import subscriptions_controller
+from utils.api import ApiResponse
 
 app = Flask(__name__)
-app.register_blueprint(products_controller)
 app.register_blueprint(product_controller)
+app.register_blueprint(products_controller)
 app.register_blueprint(subscriptions_controller)
 
 @app.route("/api/")
 def hello():
-    return api_response(
-        api_res_type["success"],
-        "Hello World!",
-        "Hello World!",
-    )
+    return ApiResponse.success('Hello world!')
