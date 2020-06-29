c57bd15a8f9e127ce2ec597b6b78aa6f8ce557c9
robertschaap <robert.schaap@mac.com>
2020-06-29 17:57:34 +0200

    refactor(stubs): squash import statements

--- utils/stubs.py
+++ utils/stubs.py
@@ -1,27 +1,21 @@
-def get_products_stub():
-    import json
+import json
 
+def get_products_stub():
     with open("stubs/products.json") as f:
         products_json = json.load(f)
 
     return products_json
 
 def create_products_stub(products):
-    import json
-
     with open("stubs/products.json", 'w') as f:
         json.dump(products, f, indent=2)
 
 def get_subscriptions_stub():
-    import json
-
     with open("stubs/subscriptions.json") as subscriptions_stub:
         subscriptions_json = subscriptions_stub.read()
 
     return json.loads(subscriptions_json)
 
 def create_subscriptions_stub(subscriptions):
-    import json
-
     with open("stubs/subscriptions.json", 'w') as f:
         json.dump(subscriptions, f, indent=2)
