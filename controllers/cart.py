bf0f073ea6989dbb92a507b49e49f9a12bf93e54
robertschaap <robert.schaap@mac.com>
2020-07-07 19:00:32 +0200

    feat(cart): setup basic controller

---
+++ controllers/cart.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+from utils.api import ApiResponse
+
+cart_controller = Blueprint('cart_controller', __name__)
+
+@cart_controller.route('/api/cart/<id>')
+def get_cart():
+    return ApiResponse.error('Cart could not be found')
+
+@cart_controller.route('/api/cart/<id>/item', methods=["PATCH"])
+def patch_cart_item():
+    # try to call a create cart route if no cart ID
+    # then try to add a product
+    return ApiResponse.error('Cart item could not be added')
+
+@cart_controller.route('/api/cart/<id>/item/<item_id>', methods=["DELETE"])
+def delete_cart_item():
+    return ApiResponse.error('Cart item could not be deleted')
+
+@cart_controller.route('/api/cart/order', methods=["POST"])
+def post_cart_order():
+    return ApiResponse.error('Cart order could not be placed')
