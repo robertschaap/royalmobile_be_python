b45e64f75b7ea03dfd011a59ca8475ef42087f9f
robertschaap <robert.schaap@mac.com>
2020-10-04 10:42:58 +0200

    feat(product): safeguard get product method in model

--- models/product.py
+++ models/product.py
@@ -2,7 +2,10 @@ from utils.stubs import get_products_stub
 
 class ProductModel:
     def get_product(self, product_id):
-        products_stub = get_products_stub()
-        product = [x for x in products_stub if x["modelId"] == product_id][0]
+        products_stub = get_products_stub() or []
+        product = [x for x in products_stub if x["modelId"] == product_id]
 
-        return product
+        if len(product) != 0:
+            return product[0]
+
+        raise ValueError("Product could not be found")
