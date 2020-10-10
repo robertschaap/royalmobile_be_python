b1f4f97ca98ea8b691012925a9f7856f03bd4760
robertschaap <robert.schaap@mac.com>
2020-10-10 19:14:31 +0200

    feat(server): register cart controller

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from controllers.cart import cart_controller
 from controllers.product import product_controller
 from controllers.products import products_controller, get_products
 from controllers.subscriptions import subscriptions_controller
 from utils.api import ApiResponse
 
 app = Flask(__name__)
+app.register_blueprint(cart_controller)
 app.register_blueprint(product_controller)
 app.register_blueprint(products_controller)
 app.register_blueprint(subscriptions_controller)
