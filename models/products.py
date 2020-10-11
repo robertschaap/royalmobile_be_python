e51bc984b8042ef08199fd276b77c959016c293d
robertschaap <robert.schaap@mac.com>
2020-10-11 11:10:41 +0200

    feat(products) implement database connection in model

--- models/products.py
+++ models/products.py
@@ -1,5 +1,6 @@
 from utils.stubs import get_products_stub
+from db.connection import Connection
 
 class ProductsModel:
     def get_products(self):
-        return get_products_stub() or []
+        return Connection.products.get_products()
