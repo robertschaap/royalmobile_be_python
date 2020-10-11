990efdd5f7710970635a7eac8e64f2c3be2a2e59
robertschaap <robert.schaap@mac.com>
2020-10-11 10:10:09 +0200

    feat(cart): implement database connection in model

--- models/cart.py
+++ models/cart.py
@@ -1,12 +1,5 @@
+from db.connection import Connection
+
 class CartModel:
     def get_cart_by_id(self, id):
-        cart = {
-            'id': "cart-id",
-            'items': [],
-            'totals': {
-                'monthly_price': "",
-                'onetime_price': "",
-            }
-        }
-
-        return cart
+        return Connection.carts.get_cart_by_id()
