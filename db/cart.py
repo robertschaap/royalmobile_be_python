da454f155b9d5d565712df53879e0efeef7a6b42
robertschaap <robert.schaap@mac.com>
2020-10-12 22:09:41 +0200

    feat(cart): rework get_cart_by_id in db

--- db/cart.py
+++ db/cart.py
@@ -1,14 +1,17 @@
-class Cart:
-    carts = [
-        {
-            'id': "new-cart",
-            'items': [],
-            'totals': {
-                'monthly_price': "",
-                'onetime_price': "",
-            }
-        }
-    ]
+test_cart = {
+    'id': "new-cart",
+    'items': [],
+    'totals': {
+        'monthly_price': "",
+        'onetime_price': "",
+    }
+}
 
-    def get_cart_by_id(self):
-        return self.carts[0]
+class Cart:
+    carts = []
+
+    def __init__(self):
+        self.carts.append(test_cart)
+
+    def get_cart_by_id(self, id):
+        return next((x for x in self.carts if x["id"] == id), None)
