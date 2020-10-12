beb1fb74fd6cd1bb5beaa7fc6b06d1762aa7d5eb
robertschaap <robert.schaap@mac.com>
2020-10-12 22:11:54 +0200

    refactor(products): cleaner find product in products db

--- db/products.py
+++ db/products.py
@@ -6,9 +6,5 @@ class Products:
 
     def get_product_by_model_id(self, model_id):
         products = get_products_stub() or []
-        product = [x for x in products if x['modelId'] == model_id]
 
-        if len(product) != 0:
-            return product[0]
-
-        return None
+        return next((x for x in products if x['modelId'] == model_id), None)
