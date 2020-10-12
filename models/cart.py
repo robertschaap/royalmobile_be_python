3f402f85d4a21ffe9447c5369c916b61a125c2e8
robertschaap <robert.schaap@mac.com>
2020-10-12 22:09:03 +0200

    feat(cart): pass id to get cart id in model and handle response

--- models/cart.py
+++ models/cart.py
@@ -1,8 +1,13 @@
 from db import Connection
 
 class CartModel:
-    def get_cart_by_id(self, id):
-        return Connection.carts.get_cart_by_id()
+    def get_cart_by_id(self, cart_id):
+        cart = Connection.carts.get_cart_by_id(cart_id)
+
+        if cart != None:
+            return cart
+
+        raise ValueError("Cart could not be found")
 
     def post_order(self, cart_id):
         # TODO; implement properly once FE is ready for it
