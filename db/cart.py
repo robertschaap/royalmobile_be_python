9b8d8dedaf1947eeed34e9df19c85754f4a304e7
robertschaap <robert.schaap@mac.com>
2020-10-13 18:17:08 +0200

    refactor(cart): move new cart into class

--- db/cart.py
+++ db/cart.py
@@ -1,19 +1,17 @@
 import uuid
 
-test_cart = {
-    'id': "new-cart",
-    'items': [],
-    'totals': {
-        'monthly_price': "",
-        'onetime_price': "",
-    }
-}
-
 class Cart:
     carts = []
 
     def __init__(self):
-        self.carts.append(test_cart)
+        self.carts.append({
+            'id': "new-cart",
+            'items': [],
+            'totals': {
+                'monthly_price': "",
+                'onetime_price': "",
+            }
+        })
 
     def get_cart_by_id(self, id):
         return next((x for x in self.carts if x["id"] == id), None)
