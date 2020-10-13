e34433a46bdaa729b6b65f12fd591169686b89a4
robertschaap <robert.schaap@mac.com>
2020-10-13 18:06:06 +0200

    feat(subscriptions): add get by id method to db

--- db/subscriptions.py
+++ db/subscriptions.py
@@ -3,3 +3,8 @@ from utils.stubs import get_subscriptions_stub
 class Subscriptions:
     def get_subscriptions(self):
         return get_subscriptions_stub or []
+
+    def get_subscription_by_id(self, subscription_id):
+        subscriptions = get_subscriptions_stub or []
+
+        return next((x for x in subscriptions if x["subscriptionId"] == subscription_id), None)
