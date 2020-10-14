d149ca13e64432f9160ea24da3dc116955f1bbc0
robertschaap <robert.schaap@mac.com>
2020-10-14 20:06:02 +0200

    feat(stubs): update products stub

--- stubs/products.py
+++ stubs/products.py
@@ -44,36 +44,40 @@ class ProductVariantFactory():
 def create_product(manufacturer, model, model_id):
     product = ProductFactory.create(manufacturer, model, model_id)
 
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'lime', '#7ec09a', '16gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'purple', '#8097c2', '16gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#bae596', '16gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'pink', '#d59a8d', '16gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#d8efd5', '64gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'white', '#f5f5f7', '64gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'red', '#d82e2e', '64gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'black', '#25212b', '64gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'blue', '#023b63', '64gb'))
 
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'lime', '#7ec09a', '32gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'purple', '#8097c2', '32gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#bae596', '32gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'pink', '#d59a8d', '32gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#d8efd5', '128gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'white', '#f5f5f7', '128gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'red', '#d82e2e', '128gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'black', '#25212b', '128gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'blue', '#023b63', '128gb'))
 
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'lime', '#7ec09a', '64gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'purple', '#8097c2', '64gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#bae596', '64gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'pink', '#d59a8d', '64gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#d8efd5', '256gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'white', '#f5f5f7', '256gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'red', '#d82e2e', '256gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'black', '#25212b', '256gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'blue', '#023b63', '256gb'))
 
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'lime', '#7ec09a', '128gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'purple', '#8097c2', '128gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#bae596', '128gb'))
-    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'pink', '#d59a8d', '128gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'green', '#d8efd5', '512gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'white', '#f5f5f7', '512gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'red', '#d82e2e', '512gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'black', '#25212b', '512gb'))
+    product['variants'].append(ProductVariantFactory.create(product['modelId'], 'blue', '#023b63', '512gb'))
 
     return product
 
 def create_products():
     products = []
 
-    products.append(create_product('Apple', 'iPhone X1', 'apple-iphonex1'))
-    products.append(create_product('Apple', 'iPhone X2', 'apple-iphonex2'))
-    products.append(create_product('Apple', 'iPhone X3', 'apple-iphonex3'))
-    products.append(create_product('Apple', 'iPhone X4', 'apple-iphonex4'))
-    products.append(create_product('Apple', 'iPhone X5', 'apple-iphonex5'))
-    products.append(create_product('Apple', 'iPhone X6', 'apple-iphonex6'))
+    products.append(create_product('Apple', 'iPhone 12 v1', 'apple-iphone12v1'))
+    products.append(create_product('Apple', 'iPhone 12 v2', 'apple-iphone12v2'))
+    products.append(create_product('Apple', 'iPhone 12 v3', 'apple-iphone12v3'))
+    products.append(create_product('Apple', 'iPhone 12 v4', 'apple-iphone12v4'))
+    products.append(create_product('Apple', 'iPhone 12 v5', 'apple-iphone12v5'))
+    products.append(create_product('Apple', 'iPhone 12 v6', 'apple-iphone12v6'))
 
     return products
