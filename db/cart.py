23d077dcdecb449bac72d6c6ea916a68775e12ad
robertschaap <robert.schaap@mac.com>
2020-10-17 10:02:23 +0200

    feat(cart): add delete_cart_item to db

--- db/cart.py
+++ db/cart.py
@@ -16,6 +16,9 @@ class Cart:
     def get_cart_by_id(self, cart_id):
         return next((x for x in self.carts if x["id"] == cart_id), None)
 
+    def get_cart_index(self, cart_id):
+        return next((i for i, x in enumerate(self.carts) if x["id"] == cart_id), None)
+
     def create_cart(self):
         self.carts.append({
             'id': uuid.uuid4(),
@@ -36,5 +39,20 @@ class Cart:
             self.carts.append(cart)
 
             return cart
+
+        return None
+
+    def delete_cart_item(self, cart_id, item_id):
+        index = self.get_cart_index(cart_id)
+
+        if index != None:
+            cart = self.carts.pop(index)
+            item_index = next((i for i, x in enumerate(cart["items"]) if x["id"] == item_id), None)
+
+            if item_index != None:
+                cart["items"].pop(item_index)
+                self.carts.append(cart)
+
+                return cart
 
         return None
