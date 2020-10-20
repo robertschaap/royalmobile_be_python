cca40c897f1f831c1900ea2d604e010d905600ea
robertschaap <robert.schaap@mac.com>
2020-10-20 20:05:37 +0200

    feat(cart): update add_cart_item in model

--- models/cart.py
+++ models/cart.py
@@ -14,16 +14,24 @@ class CartModel:
         return Connection.carts.create_cart()
 
     def add_cart_item(self, cart_id, variant_id, subscription_id):
-        if cart_id == "new":
-            self.create_cart()
+        used_cart_id = cart_id
 
-        product = Connection.products.get_product_by_model_id("apple-iphonex1")
-        subscription = Connection.subscriptions.get_subscription_by_id("royalmobile-20gb-1year")
+        if cart_id == "new":
+            new_cart = self.create_cart()
+            used_cart_id = new_cart.get("id")
+
+        # Split off capacity and colour
+        split = variant_id.split("-")
+        model_id = "-".join(split[:2])
+
+        # TODO: don't return the whole product but only the relevant variant
+        product = Connection.products.get_product_by_model_id(model_id)
+        subscription = Connection.subscriptions.get_subscription_by_id(subscription_id)
 
         if product == None or subscription == None:
             raise ValueError("Could not add cart item")
 
-        updated_cart = Connection.carts.add_cart_item(cart_id, {
+        updated_cart = Connection.carts.add_cart_item(used_cart_id, {
             'product': product,
             'subscription': subscription,
             'totals': None,
