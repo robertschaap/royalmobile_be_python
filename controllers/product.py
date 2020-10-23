65a53f925f5799ae3a785af26c7043030e0565ed
robertschaap <robert.schaap@mac.com>
2020-10-23 21:34:11 +0200

    feat(product): add some documentation

--- controllers/product.py
+++ controllers/product.py
@@ -5,10 +5,14 @@ from utils.api import ApiResponse
 product_controller = Blueprint('product_controller', __name__)
 product_model = ProductModel()
 
-@product_controller.route('/api/product/<id>')
-def get_product(id):
+@product_controller.route('/api/product/<model_id>')
+def get_product(model_id):
+    """
+    :param str model_id: model ID `manufacturer-device`
+    :return: ApiResponse of Product or error
+    """
     try:
-        product = product_model.get_product(id)
+        product = product_model.get_product(model_id)
         return ApiResponse.success(product)
     except:
         return ApiResponse.error("Product could not be found")
