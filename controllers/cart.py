4f0496901d33122030137df183a2a9aa88b8c1ce
robertschaap <robert.schaap@mac.com>
2020-10-23 21:34:00 +0200

    feat(cart): add some documentation

--- controllers/cart.py
+++ controllers/cart.py
@@ -8,6 +8,10 @@ cart_model = CartModel()
 
 @cart_controller.route('/api/cart/<id>')
 def get_cart(id):
+    """
+    :param str id: UUIDv4 formatted as string
+    :return: ApiResponse of Cart or error
+    """
     try:
         return ApiResponse.success(cart_model.get_cart_by_id(id))
     except:
@@ -15,6 +19,12 @@ def get_cart(id):
 
 @cart_controller.route('/api/cart/<id>/item', methods=["PATCH"])
 def add_cart_item(id):
+    """
+    :param str id: either UUIDv4 formatted as string or `new`
+    :param str variantId: from request body
+    :param str subscriptionId: from request body
+    :return: ApiRespons of Cart with the added item or error
+    """
     data = request.form or request.get_json(force=True)
     variant_id = data.get('variantId')
     subscription_id = data.get('subscriptionId')
@@ -24,16 +34,24 @@ def add_cart_item(id):
     except:
         return ApiResponse.error('Could not add cart item')
 
-@cart_controller.route('/api/cart/<id>/item/<item_id>', methods=["DELETE"])
-def delete_cart_item(id, item_id):
+@cart_controller.route('/api/cart/<cart_id>/item/<item_id>', methods=["DELETE"])
+def delete_cart_item(cart_id, item_id):
+    """
+    :param str cart_id: UUIDv4 formatted as string
+    :param str item_id: UUIDv4 formatted as string
+    :return: ApiResponse of Cart without the deleted item or error
+    """
     try:
-        cart = cart_model.delete_cart_item(id, item_id)
-        return ApiResponse.success(cart_model.delete_cart_item(id, item_id))
+        cart = cart_model.delete_cart_item(cart_id, item_id)
+        return ApiResponse.success(cart_model.delete_cart_item(cart_id, item_id))
     except:
         return ApiResponse.error('Could not delete cart item')
 
 @cart_controller.route('/api/cart/order', methods=["POST"])
 def post_order():
+    """
+    :param str cart_id: from request body
+    """
     cart_id = request.values.get('cartId')
     result = cart_model.post_order(cart_id)
 
