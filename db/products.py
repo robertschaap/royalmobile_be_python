ce5ff1253d7b1a7eb8ab2d2cdc7a3a738f1fb871
robertschaap <robert.schaap@mac.com>
2020-10-25 09:21:03 +0100

    refactor(product): do not directly index dicts

--- db/products.py
+++ db/products.py
@@ -7,20 +7,20 @@ class Products:
     def get_product_by_model_id(self, model_id):
         products = self.get_products()
 
-        return next((x for x in products if x['modelId'] == model_id), None)
+        return next((x for x in products if x.get("modelId") == model_id), None)
 
     def get_product_by_variant_id(self, variant_id):
         split = variant_id.split("-")
         model_id = "-".join(split[:2])
 
         products = self.get_products()
-        product_by_model = next((x for x in products if x['modelId'] == model_id), None)
+        product_by_model = next((x for x in products if x.get("modelId") == model_id), None)
 
         if product_by_model == None:
             return None
 
         product_variants = product_by_model["variants"]
-        selected_variant = next((x for x in product_variants if x["variantId"] == variant_id), None)
+        selected_variant = next((x for x in product_variants if x.get("variantId") == variant_id), None)
 
         if selected_variant == None:
             return None
